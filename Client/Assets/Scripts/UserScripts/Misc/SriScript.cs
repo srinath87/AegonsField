5358ed442c948beb27265c702979a3f051c0a1d2
Srinath Upadhyayula <srinathupadhyayula@yahoo.com>
2013-06-17 20:53:14 +0100

    Conflict tests continued : Merging Scripts.

--- Client/Assets/Scripts/UserScripts/Misc/SriScript.cs
+++ Client/Assets/Scripts/UserScripts/Misc/SriScript.cs
@@ -4,12 +4,19 @@ using System.Collections;
 public class SriScript : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		Debug.Log("Hello!");
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 	
+	}
+	
+	void sriFunc()
+	{
+		// Sri is Kewl ;)	
 	}
 }
