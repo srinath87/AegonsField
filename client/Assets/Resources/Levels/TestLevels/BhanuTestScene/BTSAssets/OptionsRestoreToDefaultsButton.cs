75f025f675facf9f83a34fff5f0adb89c3ba61f4
Bhanu <uabhanu@gmail.com>
2013-08-24 13:44:42 +0100

    Options Menu Almost done

---
+++ client/Assets/Resources/Levels/TestLevels/BhanuTestScene/BTSAssets/OptionsRestoreToDefaultsButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionsRestoreToDefaultsButton : UIButtonSound{
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+	
+	}
+	
+	void OnClick ()
+	{
+		if (enabled && trigger == Trigger.OnClick)
+		{
+			
+		}
+	}
+	
+}
