d79e8670f901a1c8940b1a9e9db344d04244dd3f
Snky <Snky@live.co.uk>
2013-09-24 19:16:00 +0100

    hhh

---
+++ Client/Assets/Scripts/UserScripts/CardScript/CardController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardController : MonoBehaviour {
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void OnMouseDown( uint touchposition_ )
+	{
+		//Debug.Log( touchposition_ );//+ " " touchposition_.y + " " + touchposition_.z );
+	}
+	
+	
+}
